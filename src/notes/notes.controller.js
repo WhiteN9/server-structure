3f51695d706c2a933d2bc6c2151a586db5673592
White Nguyen <whitenguyen0909@gmail.com>
2022-05-27 10:13:48 -0600

    added update and destroy function to controller and router

--- src/notes/notes.controller.js
+++ src/notes/notes.controller.js
@@ -4,31 +4,34 @@ const list = (req, res) => {
   res.json({ data: notes });
 };
 
-const noteExists = (req, res, next) => {
-  const noteId = Number(req.params.noteId);
-  const foundNote = notes.find((note) => note.id === noteId);
-  if (foundNote) {
-    return next();
-  } else {
-    return next({
-      status: 404,
-      message: `Note id nonpm t found: ${req.params.noteId}`,
-    });
-  }
-};
-
-const read = (req, res, next) => {
+const read = (req, res) => {
   const noteId = Number(req.params.noteId);
   const foundNote = notes.find((note) => note.id === noteId);
   res.json({ data: foundNote });
 };
 
-const hasText = (req, res, next) => {
-  const { data: { text } = {} } = req.body;
-  if (text) {
+const noteExists = (req, res, next) => {
+  const { noteId } = req.params;
+  const foundNote = notes.find((note) => note.id === Number(noteId));
+  if (foundNote) {
     return next();
+  } else {
+    return next({
+      status: 404,
+      message: `Note id not found: ${noteId}`,
+    });
   }
-  return next({ status: 400, message: "A 'text' property is required." });
+};
+
+const bodyDataHas = (propertyName) => {
+  return (req, res, next) => {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      console.log(data[propertyName]);
+      return next();
+    }
+    return next({ status: 400, message: "A 'text' property is required." });
+  };
 };
 
 const create = (req, res, next) => {
@@ -42,4 +45,28 @@ const create = (req, res, next) => {
   res.status(201).json({ data: newNote });
 };
 
-module.exports = { list, read: [noteExists, read], create: [hasText, create] };
+const update = (req, res) => {
+  const { noteId } = req.params;
+  const foundNote = notes.find((note) => note.id === Number(noteId));
+  const { data: { id, text } = {} } = req.body;
+  foundNote.id = id;
+  foundNote.text = text;
+
+  res.json({ data: foundNote });
+};
+
+const destroy = (req, res) => {
+  const { noteId } = req.params;
+  const index = notes.findIndex((note) => note.id === Number(noteId));
+
+  notes.splice(index, 1);
+
+  res.sendStatus(204);
+};
+module.exports = {
+  list,
+  create: [bodyDataHas("text"), create],
+  read: [noteExists, read],
+  update: [noteExists, bodyDataHas("text"), update],
+  delete: [noteExists, destroy],
+};
--- src/notes/notes.router.js
+++ src/notes/notes.router.js
@@ -3,6 +3,10 @@ const controller = require("./notes.controller");
 
 router.route("/").get(controller.list).post(controller.create);
 
-router.route("/:noteId").get(controller.read);
+router
+  .route("/:noteId")
+  .get(controller.read)
+  .put(controller.update)
+  .delete(controller.delete);
 
 module.exports = router;
