3d549065ff51697555e31bb94c634027d5e0a17c
White Nguyen <whitenguyen0909@gmail.com>
2022-05-27 13:24:09 -0600

    added ratings controller and router of the list and read function

--- src/ratings/ratings.controller.js
+++ src/ratings/ratings.controller.js
@@ -1,3 +1,24 @@
-const ratings = require("../data/ratings-data");
+const path = require("path");
+const ratings = require(path.resolve("src/data/ratings-data"));
 
-module.exports = {};
+const list = (req, res) => {
+  res.json({ data: ratings });
+};
+
+const ratingExists = (req, res, next) => {
+  const { ratingId } = req.params;
+  const ratingFound = ratings.find((rating) => rating.id === Number(ratingId));
+  if (ratingFound) {
+    res.locals.rating = ratingFound;
+    next();
+  } else {
+    next({
+      status: 404,
+      message: `Rating id not found: ${ratingId}`,
+    });
+  }
+};
+const read = (req, res) => {
+  res.json({ data: res.locals.rating });
+};
+module.exports = { list, read: [ratingExists, read] };
