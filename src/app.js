bddae990228c75e8ec9d864eb2937fd52ef2bfa0
White Nguyen <whitenguyen0909@gmail.com>
2022-05-27 15:04:41 -0600

    added controller and router files for middleware and routing, added route handler and error handler for all other errors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
 const express = require("express");
-
 const app = express();
 
+const urlsRoute = require("./urls/urls.router");
+const usesRoute = require("./uses/uses.router");
+
 app.use(express.json());
+app.use("/urls", urlsRoute);
+app.use("/uses", usesRoute);
+
+//Route not found handler
+app.use((req, res, next) => {
+  next({
+    status: 404,
+    message: `Not found: ${req.originalUrl}`,
+  });
+});
+
+//Error handler
+app.use((err, req, res) => {
+  console.error(err);
+  const {
+    status = 500,
+    message = `Internal Error Service! Something went wrong!`,
+  } = err;
+  res.status(status).json({ err: message });
+});
 
 // TODO: Add code to meet the requirements and make the tests pass.
 
