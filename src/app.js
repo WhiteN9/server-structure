c80e403df5a046d07b67322bde8be60d771c3e18
White Nguyen <whitenguyen0909@gmail.com>
2022-05-27 19:51:47 -0600

    now error handler is functional, parameter requires to be exactly 'error'

--- src/app.js
+++ src/app.js
@@ -3,25 +3,19 @@ const app = express();
 
 const urlsRoute = require("./urls/urls.router");
 
-
 app.use(express.json());
 app.use("/urls", urlsRoute);
 
 //Route not found handler
-app.use((req, res) => {
-  res.status(404).send(`Not found: ${req.originalUrl}`);
+app.use((req, res, next) => {
+  next({ status: 404, message: `Not found: ${req.originalUrl}` });
 });
 
 //Error handler
-app.use((err, req, res) => {
-  console.error(err);
-  const {
-    status = 500,
-    message = `Internal Error Service! Something went wrong!`,
-  } = err;
-  res.status(status).json({ err: message });
+app.use((error, req, res, next) => {
+  // console.error("error", error);
+  const { status = 500, message = `Something went wrong!` } = error;
+  res.status(status).json({ error: message });
 });
-
-// TODO: Add code to meet the requirements and make the tests pass.
 
 module.exports = app;
