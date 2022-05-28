a9a063a002cf0cd5c61b8e21fb565fc5420144b6
White Nguyen <whitenguyen0909@gmail.com>
2022-05-27 19:11:56 -0600

    added update function for urls

--- src/urls/urls.controller.js
+++ src/urls/urls.controller.js
@@ -4,11 +4,29 @@ const list = (req, res) => {
   res.json({ data: urls });
 };
 
+let lastUrlId = urls.reduce((maxId, href) => Math.max(maxId, href.id), 0);
 const create = (req, res) => {
+  const { data: { href } = {} } = req.body;
 
+  const newUrl = {
+    id: ++lastUrlId,
+    href,
+  };
+  urls.push(newUrl);
+  res.status(201).json({ data: newUrl });
 };
+
 const read = (req, res) => {
   res.json({ data: res.locals.url });
+};
+
+const update = (req, res) => {
+  const { data: { href } = {} } = req.body;
+  const url = res.locals.url;
+
+  url.href = href;
+
+  res.json({ data: url });
 };
 
 const urlExists = (req, res, next) => {
@@ -20,13 +38,24 @@ const urlExists = (req, res, next) => {
   } else {
     next({
       status: 404,
-      message: `Url not found`,
+      message: `Url id not found: ${id}`,
     });
   }
 };
 
+const bodyHasProperty = (propertyName) => {
+  return (req, res, next) => {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({ status: 400, message: `Must include a ${propertyName}` });
+  };
+};
 
 module.exports = {
   list,
+  create: [bodyHasProperty("href"), create],
   read: [urlExists, read],
+  update: [urlExists, bodyHasProperty("href"), update],
 };
--- src/urls/urls.router.js
+++ src/urls/urls.router.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const controller = require("./urls.controller");
-// const usesRoute = require("../uses/uses.router");
+const usesRoute = require("../uses/uses.router");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list).all(methodNotAllowed);
+router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
 
-router.route("/:id").get(controller.read).all(methodNotAllowed);
+router.route("/:id").get(controller.read).put(controller.update).all(methodNotAllowed);
 module.exports = router;
