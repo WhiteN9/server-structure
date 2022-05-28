14cbcfc30387ccbf72a60e10fb1ded7c95fd68ac
White Nguyen <whitenguyen0909@gmail.com>
2022-05-27 20:02:47 -0600

    added router and controller of Uses to app.js, added delete function to uses.controller

--- src/uses/uses.controller.js
+++ src/uses/uses.controller.js
@@ -8,6 +8,16 @@ const read = (req, res) => {
   res.json({ data: res.locals.use });
 };
 
+const destroy = (req, res) => {
+  const { useId } = req.params;
+  const index = uses.findIndex((use) => use.id === Number(useId));
+
+  if(index > -1 ) {
+    uses.splice(index,1)
+  }
+  res.sendStatus(204);
+}
+
 const usesExists = (req, res, next) => {
   const id = req.params.useId;
   const foundUse = uses.find((use) => use.id === Number(id));
@@ -25,4 +35,5 @@ const usesExists = (req, res, next) => {
 module.exports = {
   list,
   read: [usesExists, read],
+  delete: [usesExists, destroy],
 };
